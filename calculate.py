10e3f415e6d78456910a4ae089b388278116b4f3
tappy <shun.tappy@gmail.com>
2020-01-24 20:23:14 +0900

    fix

--- calculate.py
+++ calculate.py
@@ -7,13 +7,11 @@ def get_forecast():
     now = datetime.datetime.today()
     standard_time = datetime.datetime(2020, 1, 23, 6, 00, 00) #rotate_mode = 0
 
-    dic = {}
+    rotate_mode = (now - standard_time).days % 3
 
     #Defence Forces schedule
     table = ['　　全兵団　　', '深碧の造魔兵団', '蒼怨の屍獄兵団', '銀甲の凶蟲兵団', '翠煙の海妖兵団', 
              '　　全兵団　　', '闇朱の獣牙兵団', '紫炎の鉄機兵団', '翠煙の海妖兵団']
-
-    rotate_mode = (now - standard_time).days % 3
 
     if rotate_mode == 0:
         pass
@@ -22,6 +20,7 @@ def get_forecast():
     elif rotate_mode == 2:
         table = table[3:] + table[:3]
 
+    dic = {}
     dic['current_group'] = table[now.hour % 9]
     dic['next_group'] = table[(now.hour + 1) % 9]
     dic['next_next_group'] = table[(now.hour + 2) % 9]
